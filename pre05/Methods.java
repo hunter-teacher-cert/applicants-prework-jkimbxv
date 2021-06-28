4e9933cec4ae160f2892d4bc33ece82b4b03bf1b
Jiyoon Kim <jkim@bronxvilleschool.org>
2021-06-28 15:21:50 -0400

    new and improved

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -9,6 +9,9 @@ public class Methods {
         System.out.println(isTriangle(1,2,3));
         System.out.print("Exercise 8: ");
         System.out.println(power(2,5));
+        System.out.print("Exercise 8 ver 2: ");
+        System.out.println(ack(3,3));
+
     }
     // exercise 2
     public static boolean isDivisible(int n, int m){
@@ -31,7 +34,7 @@ public class Methods {
       return result;
     }
 
-    //exercise 8
+    //exercise 8 (some versions differ)
     public static double power(double x, int n){
       double result;
       if (n == 0){
@@ -40,5 +43,15 @@ public class Methods {
         result = x*power(x,n-1);
       }
       return result;
+    }
+    public static int ack(int m, int n){
+      if (m==0){
+        return n+1;
+      }else if (m>0 && n==0){
+        return ack(m-1,1);
+      }else if(m>0 && n>0){
+        return (ack(m-1,ack(m,n-1)));
+      }
+      return -1;
     }
 }
